8fc21bc9e5db718625cf9580e8a1faadec98b41c
Omprakash <Omprakash@Omprakash>
2022-03-27 21:59:09 +0530

    first commit

---
+++ learningPurpose/src/test/java/learningPurpose/learn/OneAndOnly.java
@@ -0,0 +1,10 @@
+package learningPurpose.learn;
+
+import org.testng.annotations.Test;
+
+public class OneAndOnly {
+@Test
+public void suna() {
+	System.out.println("Suna sun sun sun.. bulei nemi aamar gaon");
+}
+}
